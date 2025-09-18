4a9a8127f7eaf154167ba7812e321d68b3c0dcb8
blubbaeri <mridhogha@gmail.com>
2025-09-18 13:22:20 +0700

    menambah monster dan hero

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ class Program
     {
         Karakter hero = new Karakter("Cloud", 100);
         Karakter monster = new Karakter("Nemesis", 80);
+        Karakter hero2 = new Karakter("Tifa", 90);
+        Karakter monster2 = new Karakter("Licker", 70);
 
         hero.Info();
         monster.Info();
+        hero2.Info();
+        monster2.Info();
     }
 }
 
